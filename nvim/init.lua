4276236241709527c100bec266575cf24ef29fbc
zzz <zzz@zzz.zzz>
2023-06-20 03:22:02 +0800

    Add nvim lua config

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+require("vimOption")
+require("plugins")
+require("remap")
+
+function isModuleAvailable(name)
+    if package.loaded[name] then
+        return true
+    else
+        for _, searcher in ipairs(package.searchers or package.loaders) do
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                package.preload[name] = loader
+                return true
+            end
+        end
+        return false
+    end
+end
+
+if isModuleAvailable("nvim-treesitter.configs") then
+    require("treesitterSetup")
+end
