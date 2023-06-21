9ceaf2f489a2e414f23b4ba65872194668261bba
zzz <zzz@zzz.zzz>
2023-06-21 23:19:29 +0800

    use global clipboard

--- nvim/lua/vimOption.lua
+++ nvim/lua/vimOption.lua
@@ -1,10 +1,15 @@
+-- cursor shape
 vim.opt.guicursor = ""
 
+-- line number
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- tabsize
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- clipboard
+vim.opt.clipboard = "unnamed"
